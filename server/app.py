9cc2d581bfa50939b90bee0e0a32ace0e3c1e1eb
Naomi Mogi <nyabokenaomi001@gmail.com>
2024-04-06 05:47:09 +0300

    Building get api lab compulsory

--- server/app.py
+++ server/app.py
@@ -1,16 +1,9 @@
-#!/usr/bin/env python3
-
-from flask import Flask, make_response, jsonify
-from flask_migrate import Migrate
-
+from flask import Flask, jsonify, abort
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
 
 db.init_app(app)
 
@@ -19,20 +12,26 @@ def index():
     return '<h1>Bakery GET API</h1>'
 
 @app.route('/bakeries')
-def bakeries():
-    return ''
+def get_all_bakeries():
+    bakeries = Bakery.query.all()
+    return jsonify([bakery.to_dict() for bakery in bakeries])
 
 @app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    return ''
+def get_bakery_by_id(id):
+    bakery = Bakery.query.get(id)
+    if bakery is None:
+        abort(404)
+    return jsonify(bakery.to_dict())
 
 @app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    return ''
+def get_baked_goods_by_price():
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return jsonify([baked_good.to_dict() for baked_good in baked_goods])
 
 @app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    return ''
+def get_most_expensive_baked_good():
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return jsonify(most_expensive.to_dict())
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
